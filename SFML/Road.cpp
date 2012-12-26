e5a933d3cea5716c4b4e177fc5b51b1b197d6e14
Lukas Dürrenberger <lukas@flexworld-game.com>
2012-12-26 23:32:28 +0100

    Floats should always use a .f suffix. Some other minor code clean-up.

--- SFML/Road.cpp
+++ SFML/Road.cpp
@@ -1,33 +1,37 @@
+// Warning: This code isn't nice at all, so don't even think about using it in production code. ;-)
+
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1024, 768), "Road");
 	window.setFramerateLimit(60);
 
-	sf::RectangleShape grass(sf::Vector2f(262, 768));
+	sf::RectangleShape grass(sf::Vector2f(262.f, 768.f));
 	grass.setFillColor(sf::Color(0, 100, 0));
 
-	sf::RectangleShape road(sf::Vector2f(500, 768));
+	sf::RectangleShape road(sf::Vector2f(500.f, 768.f));
 	road.setFillColor(sf::Color(50, 50, 50));
-	road.setPosition(262, 0);
+	road.setPosition(262.f, 0.f);
 
 	const float HEIGHT = 70.f;
+	const unsigned int STRIPES_COUNT = 7;
 
-	sf::RectangleShape greyLight(sf::Vector2f(1024, HEIGHT));
+	sf::RectangleShape greyLight(sf::Vector2f(1024.f, HEIGHT));
 	greyLight.setFillColor(sf::Color(50, 50, 50, 20));
 
-	sf::RectangleShape greyDark(sf::Vector2f(1024, HEIGHT));
+	sf::RectangleShape greyDark(sf::Vector2f(1024.f, HEIGHT));
 	greyDark.setFillColor(sf::Color(50, 50, 50, 50));
 
-	sf::RectangleShape stripeWhite(sf::Vector2f(14, HEIGHT));
+	sf::RectangleShape stripeWhite(sf::Vector2f(14.f, HEIGHT));
 	stripeWhite.setFillColor(sf::Color::White);
 
-	sf::RectangleShape stripeRed(sf::Vector2f(14, HEIGHT));
+	sf::RectangleShape stripeRed(sf::Vector2f(14.f, HEIGHT));
 	stripeRed.setFillColor(sf::Color::Red);
 
 	sf::Clock animation;
-	float moved = 0;
+	float moved = 0.f;
 
 	while(window.isOpen())
 	{
@@ -48,51 +52,51 @@ int main()
 
 		window.clear();
 
-		grass.setPosition(0, 0);
+		grass.setPosition(0.f, 0.f);
 		window.draw(grass);
 
-		grass.setPosition(762, 0);
+		grass.setPosition(762.f, 0.f);
 		window.draw(grass);
 
 		window.draw(road);
 
-		for(unsigned int i = 0; i < 7; ++i)
+		for(unsigned int i = 0; i < STRIPES_COUNT; ++i)
 		{
 			stripeWhite.setPosition(262.f, (moved+i*HEIGHT*2)-HEIGHT*2);
 			window.draw(stripeWhite);
 		}
 
-		for(unsigned int i = 0; i < 7; ++i)
+		for(unsigned int i = 0; i < STRIPES_COUNT; ++i)
 		{
 			stripeRed.setPosition(262.f, (moved+i*HEIGHT*2)-HEIGHT);
 			window.draw(stripeRed);
 		}
 
-		for(unsigned int i = 0; i < 7; ++i)
+		for(unsigned int i = 0; i < STRIPES_COUNT; ++i)
 		{
 			stripeWhite.setPosition(504.f, (moved+i*HEIGHT*2)-HEIGHT);
 			window.draw(stripeWhite);
 		}
 
-		for(unsigned int i = 0; i < 7; ++i)
+		for(unsigned int i = 0; i < STRIPES_COUNT; ++i)
 		{
 			stripeWhite.setPosition(748.f, (moved+i*HEIGHT*2)-HEIGHT*2);
 			window.draw(stripeWhite);
 		}
 
-		for(unsigned int i = 0; i < 7; ++i)
+		for(unsigned int i = 0; i < STRIPES_COUNT; ++i)
 		{
 			stripeRed.setPosition(748.f, (moved+i*HEIGHT*2)-HEIGHT);
 			window.draw(stripeRed);
 		}
 
-		for(unsigned int i = 0; i < 7; ++i)
+		for(unsigned int i = 0; i < STRIPES_COUNT; ++i)
 		{
 			greyLight.setPosition(0.f, (moved+i*HEIGHT*2)-HEIGHT*2);
 			window.draw(greyLight);
 		}
 
-		for(unsigned int i = 0; i < 7; ++i)
+		for(unsigned int i = 0; i < STRIPES_COUNT; ++i)
 		{
 			greyDark.setPosition(0.f, (moved+i*HEIGHT*2)-HEIGHT);
 			window.draw(greyDark);
@@ -100,6 +104,4 @@ int main()
 
 		window.display();
 	}
-
-	return 0;
 }
