0343f52e566ce6ee2899ab0b9dd8a24707cfcca8
Lukas Dürrenberger <lukas@flexworld-game.com>
2012-12-26 23:55:45 +0100

    Added a few descriptive comments.

--- SFML/FadingDots.cpp
+++ SFML/FadingDots.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
 #include <list>
 
 int main()
@@ -6,8 +8,8 @@ int main()
     sf::RenderWindow window(sf::VideoMode(500, 500), "Fading Dots");
     window.setFramerateLimit(60);
 
-    sf::Vector2f old_pos(0, 0);
-    static const int STEP = 1;
+    sf::Vector2f old_pos(0.f, 0.f);
+    const int STEP = 1;
 
     std::list<sf::CircleShape> points;
 
@@ -20,11 +22,13 @@ int main()
                 window.close();
         }
 
+        // Check if the mouse has moved
         sf::Vector2f pos = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
         if(pos != old_pos)
         {
+            // Create and add a new circle to the points list.
             sf::CircleShape cs;
-            cs.setRadius(10);
+            cs.setRadius(10.f);
             cs.setPosition(pos);
             cs.setFillColor(sf::Color::Red);
             points.push_back(cs);
@@ -34,12 +38,12 @@ int main()
 
         window.clear();
 
-        for(auto it = points.begin(); it != points.end(); it++)
+        for(auto it = points.begin(); it != points.end(); ++it)
         {
             window.draw(*it);
-            if(it->getFillColor().a-STEP < 0)
+            if(it->getFillColor().a-STEP < 0) // When the transparency falls below zero (= invisible) then erase the dot.
                 it = points.erase(it);
-            else
+            else // Otherwise draw it with a increasing green touch (turns yellowish).
                 it->setFillColor(sf::Color(255, it->getFillColor().g+STEP, 0, it->getFillColor().a-STEP));
         }
 
